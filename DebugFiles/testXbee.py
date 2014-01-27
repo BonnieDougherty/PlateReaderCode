a37f81d2b6485b31e8df93e29fc1abae29ce75fd
Bonnie Dougherty <bvd5nq@virginia.edu>
2014-01-27 16:08:44 +0000

    Test connection with coordinator XBee

---
+++ DebugFiles/testXbee.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+
+from xbee import ZigBee
+import serial
+
+BROADCAST = '\x00\x00\x00\x00\x00\x00\xFF\xFF'
+UNKNOWN = '\xFF\xFE'
+PORT = '/dev/ttyAMA0'
+BAUD_RATE = 9600
+
+ser = serial.Serial(PORT,BAUD_RATE)
+xbee = ZigBee(ser,escaped=True)
+
+if __name__ == '__main__':
+	# Code to be executed when file is called directly
+	xbee.send("at",command="MY")
+	print(xbee.wait_read_frame())
